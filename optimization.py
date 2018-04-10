4abf83c4a9ab0123932ad28cacde086f0b6a14d3
Connie-Wild <34596022+Connie-Wild@users.noreply.github.com>
2018-04-11 00:44:03 +0900

    optimizationで使用するCPUコア数を設定出来るように

--- optimization.py
+++ optimization.py
@@ -9,7 +9,7 @@ from src import channel
 from concurrent.futures import ProcessPoolExecutor
 
 def describe(params):
-    i, j, k, l, candleTerm, cost, fileName = params
+    i, j, k, l, candleTerm, cost, fileName, core = params
 
     channelBreakOut = channel.ChannelBreakOut()
     channelBreakOut.entryTerm = i[0]
@@ -23,14 +23,17 @@ def describe(params):
     channelBreakOut.candleTerm = candleTerm
     channelBreakOut.cost = cost
     channelBreakOut.fileName = fileName
-    # logging.info('================================')
-    # logging.info('[%s/%s] entryTerm:%s closeTerm:%s rangePercent:%s rangePercentTerm:%s rangeTerm:%s rangeTh:%s waitTerm:%s waitTh:%s candleTerm:%s cost:%s',len(paramList)+1,total,i[0],i[1],l[0],l[1],j[1],j[0],k[0],k[1],candleTerm,cost)
+    if core == 1:
+        logging.info('================================')
+        logging.info('entryTerm:%s closeTerm:%s rangePercent:%s rangePercentTerm:%s rangeTerm:%s rangeTh:%s waitTerm:%s waitTh:%s candleTerm:%s cost:%s',i[0],i[1],l[0],l[1],j[1],j[0],k[0],k[1],candleTerm,cost)
+    else:
+        pass
 
     #テスト
     pl, profitFactor = channelBreakOut.describeResult()
     return [pl, profitFactor, i, l, j, k]
 
-def optimization(candleTerm, cost, fileName):
+def optimization(candleTerm, cost, fileName, core):
     entryAndCloseTerm = [(2,2),(3,2),(2,3),(3,3),(4,2),(2,4),(4,3),(3,4),(4,4),(5,2),(2,5),(5,3),(3,5),(5,4),(4,5),(5,5),(10,10)]
     rangeThAndrangeTerm = [(None,3),(5000,3),(10000,3),(None,5),(5000,5),(10000,5),(None,10),(5000,10),(10000,10),(None,15),(5000,15),(10000,15),(None,None)]
     waitTermAndwaitTh = [(0,0),(3,10000),(3,15000),(3,20000),(5,10000),(5,15000),(5,20000),(10,10000),(10,15000),(10,20000),(15,10000),(15,15000),(15,20000)]
@@ -40,17 +43,20 @@ def optimization(candleTerm, cost, fileName):
     paramList = []
     params = []
     for i, j, k, l in itertools.product(entryAndCloseTerm, rangeThAndrangeTerm, waitTermAndwaitTh, rangePercentList):
-        params.append([i, j, k, l, candleTerm, cost, fileName])
+        params.append([i, j, k, l, candleTerm, cost, fileName, core])
 
-    # 同期処理
-    # for param in params:
-    #     result = describe(param)
-    #     paramList.append(result)
-
-    # 非同期処理
-    with ProcessPoolExecutor() as executor:
-        for result in executor.map(describe, params):
+    if core == 1:
+        # 同期処理
+        for param in params:
+            result = describe(param)
             paramList.append(result)
+            logging.info('[%s/%s]',len(paramList),total)
+    else:
+        # 非同期処理
+        with ProcessPoolExecutor(max_workers=core) as executor:
+            for result in executor.map(describe, params):
+                paramList.append(result)
+                logging.info('[%s/%s]',len(paramList),total)
 
     pF = [i[1] for i in paramList]
     pL = [i[0] for i in paramList]
@@ -88,5 +94,5 @@ if __name__ == '__main__':
 
     #最適化
     start = time.time()
-    optimization(candleTerm=config["candleTerm"], cost=config["cost"], fileName=config["fileName"])
+    optimization(candleTerm=config["candleTerm"], cost=config["cost"], fileName=config["fileName"], core=config["core"])
     logging.info('total processing time: %s', time.time() - start)
